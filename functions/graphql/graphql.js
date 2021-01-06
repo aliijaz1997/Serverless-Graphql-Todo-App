6e00d4a758a577e37765c5af55c3b0c4cdd68abd
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-01-06 21:28:23 +0500

    Done with backend

---
+++ functions/graphql/graphql.js
@@ -0,0 +1,72 @@
+const { ApolloServer, gql } = require("apollo-server-lambda");
+var faunadb = require("faunadb");
+q = faunadb.query;
+const typeDefs = gql`
+  type Query {
+     todo : [Todo!]
+  }
+  type Todo {
+    id : ID!
+    task : String!
+    statusofthetask : Boolean!
+  }
+  type Mutation {
+    addTask(task : String!) : Todo  
+  }
+`;
+
+const resolvers = {
+  Query: {
+    todo: async (parent, args, context) => {
+      try {
+        var client = new faunadb.Client({ secret: "fnAD-uDWGkACB2cZcY9900jX220NPb0qHvsPOXPl" });
+        var result = await client.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index("tasksindex"))),
+            q.Lambda(x => q.Get(x))
+          )
+        );
+        console.log(result)
+        return result.data.map(tasks => {
+          return {
+            task : tasks.data.task,
+            id : tasks.ts,
+            statusofthetask : tasks.data.status
+          }
+        })
+      } catch (error) {
+        console.log(`Error is ${error}` );
+      }
+    }
+  },
+  Mutation: {
+    addTask: async (_, {task}) => {
+      try {
+        var client = new faunadb.Client({ secret: "fnAD-uDWGkACB2cZcY9900jX220NPb0qHvsPOXPl" });
+        var result = await client.query(
+          q.Create(
+            q.Collection('tasks'),
+            { data : {
+              task : task,
+              statusofthetask : true
+            } },
+          )
+        );
+        console.log(result.ref.data)
+        return result.ref.data;
+      } catch (error) {
+        console.log(`Error is ${error}` );
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // playground: true,
+  // introspection: true
+});
+
+exports.handler = server.createHandler();
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,56 @@
 import React from "react"
-import { useQuery } from '@apollo/client';
+import { useMutation, useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 
 
 // This query is executed at run time by Apollo.
-const APOLLO_QUERY = gql`
+const Get_All_Tasks = gql`
 {
-  message
+  todo {
+    task,
+    id,
+    statusofthetask
+  }
 }
 `;
+const ADD_Task = gql`
+mutation addTask ( $task : String!) {
+  addTask (task : $task) {
+    task
+  }
+}
+`
 
 export default function Home() {
-  const { loading, error, data } = useQuery(APOLLO_QUERY);
-
+  const [addTask] = useMutation(ADD_Task)
+  let input;
+  const { loading, error, data } = useQuery(Get_All_Tasks);
+  // console.log(error);
+  console.log(data)
+  // console.log(addTask)
+ if (error) {
+   return `Error is ${error.message}`
+ }
   return (
       <div>
         <h2>Data Received from Apollo Client at runtime from Serverless Function:</h2>
         {loading && <p>Loading Client Side Querry...</p>}
-        {error && <p>Error: ${error.message}</p>}
-        {data && data.message && (
-          <div>{data.message}</div>
-        )}
+        <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          addTask({ variables: { task: input.value },refetchQueries: [{ query: Get_All_Tasks }] });
+          input.value = '';
+        }}
+      >
+        <input
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      </div>
       </div>
   );
     
